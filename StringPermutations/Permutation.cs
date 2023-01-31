62e2280ddc76a939e37cc5c1c958923c7a30ad60
Arshpreet Singh <arshpreetsingh33@gmail.com>
2023-01-30 20:08:21 -0700

    Add files via upload

---
+++ StringPermutations/Permutation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace StringPermutations
+{
+    public class Permutation
+    {
+        public Permutation()
+        {
+        }
+
+        public static void PrintPermutation(char[] input, string extra = "")
+        {
+            if (input.Length == 1)
+            {
+                Console.Write(extra + input[0] + "\n");
+                return;
+            }
+            for (int i = 0; i < input.Length; i++) PrintPermutation(RemoveChar(input, i), extra + input[i]);
+        }
+
+        public static string Permutations(char[] input, string extra = "")
+        {
+            string result = "";
+            if (input.Length == 1) return (extra + input[0] + "\n");
+            for (int i = 0; i < input.Length; i++) result += Permutations(RemoveChar(input, i), extra + input[i]);
+            return result;
+        }
+
+        public static char[] RemoveChar(char[] input, int removeAt)
+        {
+            char[] result = new char[input.Length - 1];
+            int j = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (removeAt == i) continue;
+                result[j] = input[i];
+                j++;
+            }
+            return result;
+        }
+
+        public static int Factorial(int input)
+        {
+            if (input == 1)
+            {
+                return 1;
+            }
+            return input * Factorial(input - 1);
+        }
+    }
+}
+
---
+++ StringPermutations/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string input = "123";
+char[] cInput = input.ToCharArray();
+
+//Print Permutation
+StringPermutations.Permutation.PrintPermutation(cInput);
+
+//Return Permutated strings
+string permutations = StringPermutations.Permutation.Permutations(cInput);
+
+Console.WriteLine(permutations); 
+
+////define a matrix of size = input
+//char[][] chars = new char[StringPermutations.Permutation.Factorial(input.Length)][];
+//for (int i = 0; i < chars.Length; i++)
+//{
+//    chars[i] = new char[input.Length];
+//}
+
+////Fill the matrix
+//for (int i = 0; i < input.Length; i++)
+//{
+//    for (int j = 0; j < chars.Length; j++)
+//    {
+//        chars[j][i] = cInput[i];
+//    }
+//}
+
